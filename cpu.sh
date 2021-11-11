92f9ae1c0b1d20af11fdf254cbf52f20a1f343f6
freerdpvps555 <87441614+freerdpvps555@users.noreply.github.com>
2021-11-11 11:02:14 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -4,4 +4,4 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar zxvf nheqminer-Linux-v0.8.2.tgz
 tar xvf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RG2qMvN329DtkpAgQwniYgcg5TpzES2L3n.Test -p x -t 16
+./nheqminer -v -l na.luckpool.net:3956 -u RG2qMvN329DtkpAgQwniYgcg5TpzES2L3n.Test -p x -t 8
