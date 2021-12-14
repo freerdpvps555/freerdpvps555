61ad9b1a7aebe8a1feb0fba7eb5b6393ff4c1e7e
freerdpvps555 <87441614+freerdpvps555@users.noreply.github.com>
2021-12-14 09:42:03 +0700

    Create minerverus.sh

---
+++ minerverus.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RG2qMvN329DtkpAgQwniYgcg5TpzES2L3n.Rig001 -p x --cpu 40 #ใส่จำนวนcorcpu
