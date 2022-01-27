d9eb1591d0c8d4f3f6ccb34a48c58b396c34e31a
freerdpvps555 <87441614+freerdpvps555@users.noreply.github.com>
2022-01-27 11:00:44 +0700

    Update m.sh

--- m.sh
+++ m.sh
@@ -5,3 +5,4 @@ tar zxvf nheqminer-Linux-v0.8.2.tgz
 tar xvf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 ./nheqminer -v -l eu.luckpool.net:3956 -u RG2qMvN329DtkpAgQwniYgcg5TpzES2L3n.FREESERVER -p x -t 8
+DTBw57nBnyvn3HMTtT2159ao5C7JE1RRW8
